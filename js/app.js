a93a1821e1c86ce635ab7470e8bdbf3495a8b7af
VinKE3 <harquin09@gmail.com>
2021-11-17 22:27:17 -0300

    commmit7

--- js/app.js
+++ js/app.js
@@ -31,7 +31,76 @@ function agregarCurso(e) {
   }
 }
 
-// Lee los datos del curso // funcion + objeto
+// objeto constructor Curso// DESAFIO CODER
+function cursosnuevos(nombre, duracion, fecha) {
+  this.nombre = nombre;
+  this.duracion = duracion;
+  this.fecha = fecha;
+
+  this.datoscurso = function () {
+    console.log("el nombre del curso es: " + this.nombre);
+    console.log("la duracion del curso es de " + this.duracion);
+    console.log("la fecha estimada del curso es el " + this.fecha);
+  };
+}
+
+alert("ingrese datos del curso nuevo que desea que se incluya");
+
+let nombreCurso = prompt("ingrese nombre del curso nuevo");
+let duracionCurso = prompt("ingrese duracion estimada del curso");
+let fechaCurso = prompt("ingrese fecha estimada");
+
+let cursonuevo = new cursosnuevos(nombreCurso, duracionCurso, fechaCurso);
+
+cursonuevo.datoscurso();
+
+//ARRAY DE CURSOS/ DESAFIO CODER
+
+let nuevosCursosVerano = [];
+let nombreCursoVerano;
+let duracionCursoVerano;
+let fechaCursoVerano;
+
+for (let i = 0; i < 3; i++) {
+  nombreCursoVerano = prompt("ingrese nombre del curso de verano");
+  nuevosCursosVerano.push(nombreCursoVerano);
+
+  duracionCursoVerano = prompt("ingrese nombre del curso de verano");
+  nuevosCursosVerano.push(duracionCursoVerano);
+
+  fechaCursoVerano = prompt("ingrese fecha del curso de verano");
+  nuevosCursosVerano.push(fechaCursoVerano);
+}
+
+console.log(nuevosCursosVerano);
+
+//ARRAY DE OBJETOS/ DESAFIO CODER
+
+
+let listaCursoDiciembre = [];
+
+for (let i = 0; i < 3; i++) {
+  let nombreCursoDiciembre = prompt("ingrese nombre del curso de diciembre");
+
+  listaCursoDiciembre.push(new cursoDiciembre(nombreCursoDiciembre));
+}
+
+listaCursoDiciembre.sort((a, b) => {
+  if (a.nombreCursoDiciembre < b.nombreCursoDiciembre) {
+    return -1;
+  }
+
+  if (a.nombreCursoDiciembre > b.nombreCursoDiciembre) {
+    return 1;
+  }
+
+  return 0;
+});
+
+console.log(listaCursoDiciembre);
+
+
+// Lee los datos del curso / Funcion + Objeto
 function leerDatosCurso(curso) {
   const infoCurso = {
     imagen: curso.querySelector("img").src,
@@ -60,3 +129,5 @@ function leerDatosCurso(curso) {
   // console.log(articulosCarrito)
   carritoHTML();
 }
+
+//array de cursos
